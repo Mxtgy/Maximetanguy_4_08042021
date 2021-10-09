f5e748dadf94980c6c6e0b9aac05a2f8aa103ce7
Mxtgy <maxime.tanguy22@gmail.com>
2021-10-09 17:28:10 +0200

    Update css & files

--- modal.js
+++ modal.js
@@ -11,6 +11,7 @@ function editNav() {
 const modalbg = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
 const formData = document.querySelectorAll(".formData");
+const close = document.querySelector("#close");
 
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
@@ -18,6 +19,12 @@ modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
 // launch modal form
 function launchModal() {
   modalbg.style.display = "block";
+}
+
+// close modal form
+close.addEventListener("click", closeModal);
+function closeModal() {
+  modalbg.classList.add("remove");
 }
 
 
